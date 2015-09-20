8d98e7df0f975fa7984ba31b5363dccc61f8db13
Thomas Papillon <tp@irrealis.me>
2015-09-20 16:45:54 +0200

    Pythonize the site
    * Pythonize the generation of the page
    * Write specific infos in YAML files

--- README.md
+++ README.md
@@ -7,4 +7,55 @@ Python-generated static website, with `materialize.css` design.
 * Clone the repo
 * `virtualenv --python=python3 venv`
 * `source venv/bin/activate`
+* `python generate.py`
 
+# Structure of the repo
+
+* `content/` author files and item files (each item is either a conf, a talk, or a training)
+* `generate.py` main generation script
+* `templates/` templates directory
+* `web/` output directory, contains the static files (images, js and
+  so on), and the `.html` files after generation.
+
+# How to add an author file
+
+* Add the right `.yaml` entry in `content/authors`
+* The name of the file is understood as the author nick by the generator
+* The file should respect the following schema:
+
+    ```
+    name: Complete Name
+    email: some@email.com
+    picture: mdr.png
+    ```
+
+* Please bear in mind that the author picture will be fetched in the
+  `web/img/authors/` directory.
+
+# How to add a training/talk/conference
+
+* Add the right `.yaml` entry in the concerned subfolder of `content/`
+* The name of the file doesn't really matter
+* The file should respect the following schema:
+
+    ```
+    type: either conference, training, or talk
+    title: A title
+    date: the date of the item
+    image: lololol.png # will be printed out in the item list, and fetched in web/img/
+    short: a short and catchy summary for the talk/conf/..
+    replay_id: a Youtube ID for the replay (or nothing if there's none)
+    authors:
+      - author1
+      - author2
+    description: a full description
+    links:
+      - type: video, or http, or book
+        text: link text
+        link: proper link
+        source: youtube, NASA, whatever
+        author: you know the drill
+    slides:
+      link: http://link_to_online_version_of_slides.com
+      git: http://link_to_source_of_slides.org
+    ```
---
+++ generate.py
@@ -0,0 +1,92 @@
+import os
+import calendar
+import time
+import subprocess
+
+from jinja2 import Environment, PackageLoader
+from slugify import slugify
+import yaml
+
+class AtillaLearn:
+    output_dir = 'web/'
+    authors_files = 'content/authors/'
+    items_dirs = map(lambda d: 'content/' + d, ['conferences', 'talks', 'trainings'])
+    
+    def __init__(self):
+        # Jinja stuff
+        self.env = Environment(loader=PackageLoader('generate', 'templates'))
+
+        # Content stuff
+        self.authors = {} # nick -> infos dict
+        self.items = {} # slug -> infos dict
+
+        self.collect_authors()
+        self.collect_items()
+        
+        # Nerd stuff
+        self.nerd_dict = {
+            'gen_time': calendar.timegm(time.gmtime()),
+            'git_sha1': subprocess.check_output(
+                ['git', 'rev-parse', 'HEAD'],
+                universal_newlines=True
+            ).strip()
+        }
+
+    def collect_authors(self):
+        for authorfile in os.listdir(self.authors_files):
+            with open(self.authors_files + authorfile) as f:
+                self.authors[authorfile.split('.')[0]] = yaml.load(f.read())
+
+    def collect_items(self):
+        for dir_ in self.items_dirs:
+            for item_file in os.listdir(dir_):
+                with open(dir_ + '/' + item_file) as f:
+                    d = yaml.load(f.read())
+                    self.items[slugify(item_file.split('.')[0])] = d
+
+    def render_home(self):
+        template = self.env.get_template('index.html')
+        with open(self.output_dir + 'index.html', 'w') as f:
+            f.write(template.render(title='Atilla Learn', **self.nerd_dict))
+
+    def render_landpage(self, type_, filename, title):
+        entries = {
+            k: v for k, v in self.items.items()
+            if v['type'] == type_
+        }
+        template = self.env.get_template(filename)
+        with open(self.output_dir + filename, 'w') as f:
+            f.write(template.render(landpage_title=title, title=title, entries=entries, **self.nerd_dict))
+
+    def render_item(self, slug, entry):
+        if entry['type'] == 'conference':
+            tpl = 'conference.html'
+        elif entry['type'] == 'talk':
+            tpl = 'talk.html'
+        else:
+            tpl = 'training.html'
+
+        authors = {
+            k: v for k, v in self.authors.items()
+            if k in entry['authors']
+        }
+
+        template = self.env.get_template(tpl)
+        with open(self.output_dir + slug + '.html', 'w') as f:
+            f.write(template.render(entry=entry, authors=authors, **self.nerd_dict))
+
+    def render(self):
+        self.render_home()
+        self.render_landpage('conference', 'conferences.html', 'Conférences')
+        self.render_landpage('training', 'trainings.html', 'Formations')
+        self.render_landpage('talk', 'talks.html', 'Talks')
+        for slug, entry in self.items.items():
+            self.render_item(slug, entry)
+        
+
+if __name__ == '__main__':
+    a = AtillaLearn()
+    a.render()
+    
+
+
